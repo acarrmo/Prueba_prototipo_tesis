7cf46c9e1ee76cd52aa71b8c8be2012ca04a34ac
Angélica CarrMo <42524889+acarrmo@users.noreply.github.com>
2020-09-21 16:16:44 -0500

    Interfaz modificada
    Modificación de interfaz resultados.

---
+++ js/words.js
@@ -0,0 +1,37 @@
+
+
+    var words = [
+        {text: ".NET", weight: 13},
+        {text: "C++", weight: 16.5},
+        {text: "Java", weight: 9.4},
+        {text: "Ruby", weight: 8},
+        {text: "Python", weight: 6.2},
+        {text: "PHP", weight: 5},
+        {text: "Pascal", weight: 5},
+        {text: "Javascript", weight: 5},
+        {text: "HTML5", weight: 5},
+        {text: "Ensamblador", weight: 5},
+        {text: "Lorem", weight: 13},
+        {text: "Ipsum", weight: 10.5},
+        {text: "Dolor", weight: 9.4},
+        {text: "Sit", weight: 8},
+        {text: "Amet", weight: 6.2},
+        {text: "Consectetur", weight: 5},
+        {text: "Adipiscing", weight: 5},
+        {text: "Java", weight: 9.4},
+        {text: "Ruby", weight: 8},
+        {text: "Python", weight: 6.2},
+        {text: "PHP", weight: 5},
+        {text: "Pascal", weight: 5},
+        {text: "Javascript", weight: 5},
+        {text: "HTML5", weight: 5},
+        {text: "Ensamblador", weight: 5}
+      /* ... */
+    ];
+
+   $('#demo').jQCloud(words);
+/*
+    $(function() {
+    $('#demo').jQCloud(words);
+});
+*/
